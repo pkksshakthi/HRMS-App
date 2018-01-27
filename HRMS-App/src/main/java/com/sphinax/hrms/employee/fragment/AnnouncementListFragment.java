12435257a212125061df43e9f95aa7b999e6ae3b
ganesaka <pkksshakthi@gmail.com>
2018-01-28 01:22:42 +0530

    Announcment screen

--- HRMS-App/src/main/java/com/sphinax/hrms/employee/fragment/AnnouncementListFragment.java
+++ HRMS-App/src/main/java/com/sphinax/hrms/employee/fragment/AnnouncementListFragment.java
@@ -2,7 +2,6 @@ package com.sphinax.hrms.employee.fragment;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,10 +14,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.sphinax.hrms.R;
 import com.sphinax.hrms.global.Constants;
+import com.sphinax.hrms.global.Global;
 import com.sphinax.hrms.model.Ajax;
 import com.sphinax.hrms.model.CompanyData;
 import com.sphinax.hrms.servicehandler.ServiceCallback;
@@ -26,78 +25,39 @@ import com.sphinax.hrms.servicehandler.WebServiceHandler;
 import com.sphinax.hrms.utils.HRMSNetworkCheck;
 import com.sphinax.hrms.utils.Utility;
 import com.sphinax.hrms.view.AnnouncementListAdapter;
-import com.sphinax.hrms.view.LeaveTypeListAdapter;
-import com.sphinax.hrms.view.LeaveTypeSpinnerAdapter;
-import com.sphinax.hrms.view.QueryListAdapter;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AnnouncementListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AnnouncementListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AnnouncementListFragment extends Fragment implements AdapterView.OnItemSelectedListener,View.OnClickListener{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AnnouncementListFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    static final String[] Months = new String[]{"January", "February",
+            "March", "April", "May", "June", "July", "August", "September",
+            "October", "November", "December"};
+    private static final String TAG = "AnnouncementListFragment-";
     private static Context context;
+    private final WebServiceHandler webServiceHandler = new WebServiceHandler();
+    ArrayList<String> years = new ArrayList<String>();
     private View mView;
     private Button bt_display;
-    private Spinner sp_year,sp_month;
+    private Spinner sp_year, sp_month;
     private ListView lv_announcement;
     private ProgressDialog pdia;
-    private final WebServiceHandler webServiceHandler = new WebServiceHandler();
     private ArrayList<Ajax> announmentList;
     private AnnouncementListAdapter announcementListAdapter;
-
-    static final String[] Months = new String[] { "January", "February",
-            "March", "April", "May", "June", "July", "August", "September",
-            "October", "November", "December" };
     private int year = 0;
     private int month = 0;
-    ArrayList<String> years = new ArrayList<String>();
+    private boolean firstTime = true;
+
+
     public AnnouncementListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AnnouncementListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AnnouncementListFragment newInstance(String param1, String param2) {
-        AnnouncementListFragment fragment = new AnnouncementListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -112,12 +72,38 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
         // super.onViewCreated(view, savedInstanceState);
         mView = view;
         context = view.getContext();
+        loadComponent();
+        setListeners();
+        loadSpinnerData();
+        loadSpinnerCurrentDate();
+        fetchAnnouncementList();
+    }
 
-        bt_display = (Button) mView.findViewById(R.id.bt_display);
-        sp_month = (Spinner) mView.findViewById(R.id.sp_month);
-        sp_year = (Spinner) mView.findViewById(R.id.sp_year);
-        lv_announcement = (ListView) mView.findViewById(R.id.lv_announcement_data);
+    private void loadComponent() {
+        bt_display = mView.findViewById(R.id.bt_display);
+        sp_month = mView.findViewById(R.id.sp_month);
+        sp_year = mView.findViewById(R.id.sp_year);
+        lv_announcement = mView.findViewById(R.id.lv_announcement_data);
+    }
 
+    private void setListeners() {
+        sp_month.setOnItemSelectedListener(this);
+        sp_year.setOnItemSelectedListener(this);
+        bt_display.setOnClickListener(this);
+    }
+
+    private void loadSpinnerCurrentDate() {
+        Calendar c = Calendar.getInstance();
+        int yearTemp = c.get(Calendar.YEAR);
+        int monthTemp = c.get(Calendar.MONTH);
+        year = years.indexOf(String.valueOf(yearTemp));
+        month = monthTemp + 1;
+        sp_year.setSelection(year);
+        sp_month.setSelection(monthTemp);
+
+    }
+
+    private void loadSpinnerData() {
         years = new ArrayList<String>();
         int thisYear = Calendar.getInstance().get(Calendar.YEAR);
         for (int i = 2000; i <= thisYear; i++) {
@@ -126,51 +112,17 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, years);
         sp_year.setAdapter(adapter);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_item,Months);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, Months);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         sp_month.setAdapter(arrayAdapter);
-        setListeners();
-        fetchAnnouncementList();
 
-
-    }
-
-    private void setListeners() {
-        sp_month.setOnItemSelectedListener(this);
-        sp_year.setOnItemSelectedListener(this);
-        bt_display.setOnClickListener(this);
-    }
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_display:
-                Log.d("Year"," " + years.get(year));
-                Log.d("month"," " + Months[month]);
                 fetchAnnouncementList();
                 break;
 
@@ -184,7 +136,7 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
                 year = position;
                 break;
             case R.id.sp_month:
-                month = position;
+                month = position + 1;
                 break;
 
 
@@ -196,24 +148,10 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
 
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 
     private void fetchAnnouncementList() {
         if (!HRMSNetworkCheck.checkInternetConnection(context)) {
-            Utility.showToastMessage(context, getResources().getString(R.string.invalidInternetConnection));
+            Utility.showCustomToast(context, mView, getResources().getString(R.string.invalidInternetConnection));
             return;
         }
         pdia = new ProgressDialog(context);
@@ -223,12 +161,12 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
         }
         try {
             HashMap<String, String> requestMap = new HashMap<String, String>();
-            requestMap.put("compId",Utility.getPreference(getActivity()).getString(Constants.PREFS_COMPANY_ID, "") );
-            requestMap.put("month","10" );
-            requestMap.put("year","2017");
-            requestMap.put("branch","10005" );
-            requestMap.put("empId","10000");
-            requestMap.put("deptId","2");
+            requestMap.put("compId", Utility.getPreference(getActivity()).getString(Constants.PREFS_COMPANY_ID, ""));
+            requestMap.put("month", String.valueOf(month));
+            requestMap.put("year", years.get(year));
+            requestMap.put("branch", String.valueOf(Global.getUserInfoData().getEmpBranchId()));
+            requestMap.put("empId", Global.getLoginInfoData().getUserId());
+            requestMap.put("deptId", String.valueOf(Global.getLoginInfoData().getDeptId()));
 
             webServiceHandler.getAnnouncementList(getActivity(), context, requestMap, new ServiceCallback() {
 
@@ -248,8 +186,7 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
                     CompanyData companyData = (CompanyData) obj;
                     announmentList = new ArrayList<>();
                     announmentList = (ArrayList<Ajax>) companyData.getAjax();
-                    Log.d("ajaxList", "size --> " + announmentList.size());
-
+                    Log.d(TAG, "size --> " + announmentList.size());
 
                     announcementListAdapter = new AnnouncementListAdapter(getActivity(), announmentList);
                     lv_announcement.setAdapter(announcementListAdapter);
@@ -268,7 +205,6 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
                     if (pdia != null) {
                         pdia.dismiss();
                     }
-                    //  Utility.callServerNotResponding(context);
                 }
 
                 @Override
@@ -276,7 +212,6 @@ public class AnnouncementListFragment extends Fragment implements AdapterView.On
                     if (pdia != null) {
                         pdia.dismiss();
                     }
-                    //  Utility.callMobileVerification(activity, context);
                 }
 
             });
